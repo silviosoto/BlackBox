1033f369a9977274d34d727a5dc98cb2df149d20
ingEmprende <silviojsoto@gmail.com>
2018-03-19 02:51:08 +0000

    creacion del componente gestion usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import  {LoginComponent}  from './componentes/login/login.component';
 import { NotfoundpageComponent } from './componentes/notfoundpage/notfoundpage.component';
 import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 import {NavComponent} from './componentes/nav/nav.component';
+import { GestionUsuarioComponent } from './componentes/gestion-usuario/gestion-usuario.component';
 
 import {AuthGuard} from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'GestionUsuario', component: GestionUsuarioComponent, canActivate: [AuthGuard]},
   {path: '**', component: NotfoundpageComponent}
 ];
 
--- src/app/componentes/gestion-usuario/gestion-usuario.component.ts
+++ src/app/componentes/gestion-usuario/gestion-usuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../servicios/auth.service';
+
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-gestion-usuario',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestion-usuario.component.scss']
 })
 export class GestionUsuarioComponent implements OnInit {
+    
+  public isLogin: boolean;
+  public nombreUsuario: string;
+  public emailUsuario: string;
+  public fotoUsuario: string;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
+    $('.button-collapse').sideNav();
+
+    $('.collapsible').collapsible();
+    
+    $('select').material_select(); 
+
+    this.authService.getAuth().subscribe( auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.nombreUsuario = auth.displayName;
+        this.emailUsuario = auth.email;
+        this.fotoUsuario = auth.photoURL;
+      } else {
+        this.isLogin = false;
+      }
+    });
+  }
+  onClickLogout() {
+    this.authService.logout();
   }
 
 }
